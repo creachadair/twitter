bfb3822e0951b8d3901ccb91378291ffeb05a691
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-08-23 11:23:53 -0700

    Add package users for user lookup queries.

---
+++ users/users.go
@@ -0,0 +1,79 @@
+// Package users supports queries for user lookup.
+package users
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/creachadair/twitter"
+	"github.com/creachadair/twitter/types"
+)
+
+// Lookup constructs a lookup query for one or more users by ID.
+func Lookup(id string, opts *LookupOpts) LookupQuery {
+	return newLookup("users", "ids", id, opts)
+}
+
+// LookupByName constructs a lookup query for one or more users by username.
+func LookupByName(name string, opts *LookupOpts) LookupQuery {
+	return newLookup("users/by", "usernames", name, opts)
+}
+
+func newLookup(method, param, key string, opts *LookupOpts) LookupQuery {
+	req := &twitter.Request{
+		Method: method,
+		Params: make(twitter.Params),
+	}
+	req.Params.Add(param, key)
+	opts.addRequestParams(param, req)
+	return LookupQuery{request: req}
+}
+
+// A LookupQuery performs a lookup query for one or more users.
+type LookupQuery struct {
+	request *twitter.Request
+}
+
+// Invoke executes the query on the given context and client.
+func (q LookupQuery) Invoke(ctx context.Context, cli *twitter.Client) (*LookupReply, error) {
+	rsp, err := cli.Call(ctx, q.request)
+	if err != nil {
+		return nil, err
+	}
+	var users types.Users
+	if err := json.Unmarshal(rsp.Data, &users); err != nil {
+		return nil, fmt.Errorf("decoding user data: %v", err)
+	}
+	return &LookupReply{
+		Reply: rsp,
+		Users: users,
+	}, nil
+}
+
+// A LookupReply is the response from a LookupQuery.
+type LookupReply struct {
+	*twitter.Reply
+	Users types.Users
+}
+
+// LookupOpts provide parameters for user lookup. A nil *LookupOpts provides
+// empty values for all fields.
+type LookupOpts struct {
+	Keys       []string // additional user keys to query
+	ByUsername bool     // query by usernames instead of ID
+
+	Expansions  []string
+	TweetFields []string
+	UserFields  []string
+}
+
+func (o *LookupOpts) addRequestParams(param string, req *twitter.Request) {
+	if o == nil {
+		return // nothing to do
+	}
+	req.Params.Add(param, o.Keys...)
+	req.Params.Add(types.Expansions, o.Expansions...)
+	req.Params.Add(types.TweetFields, o.TweetFields...)
+	req.Params.Add(types.UserFields, o.UserFields...)
+}
