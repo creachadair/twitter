27a0edeba839215b573186e9e6cda0f02358b9b3
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-08-25 13:43:22 -0700

    Clean up manual tests.
    - Put network access behind a flag.
    - Make diagnostic output more readable and reduce spam.

--- twitter_test.go
+++ twitter_test.go
@@ -4,6 +4,7 @@ package twitter_test
 
 import (
 	"context"
+	"flag"
 	"os"
 	"testing"
 	"time"
@@ -14,6 +15,19 @@ import (
 	"github.com/creachadair/twitter/users"
 )
 
+var doManual = flag.Bool("manual", false, "Run manual tests that query the network")
+
+func newClient(t *testing.T) *twitter.Client {
+	return &twitter.Client{
+		Authorize: checkAuth(t),
+		Log: func(tag, msg string) {
+			if tag == "RequestURL" {
+				t.Logf("API %s :: %s", tag, msg)
+			}
+		},
+	}
+}
+
 func checkAuth(t *testing.T) twitter.Authorizer {
 	t.Helper()
 	bearerToken := os.Getenv("BEARER_TOKEN")
@@ -23,17 +37,17 @@ func checkAuth(t *testing.T) twitter.Authorizer {
 	return twitter.BearerTokenAuthorizer(bearerToken)
 }
 
-func logFunc(t *testing.T) func(tag, message string) {
-	return func(tag, message string) {
-		t.Logf("LOG tag=%s :: %s", tag, message)
+func checkManual(t *testing.T) {
+	t.Helper()
+	if !*doManual {
+		t.Skip("Skipping manual test because -manual=false")
 	}
 }
 
+// Verify that the direct call plumbing works.
 func TestClientCall(t *testing.T) {
-	cli := &twitter.Client{
-		Authorize: checkAuth(t),
-		Log:       logFunc(t),
-	}
+	checkManual(t)
+	cli := newClient(t)
 
 	rsp, err := cli.Call(context.Background(), &twitter.Request{
 		Method: "/users/by/username/jack",
@@ -59,67 +73,63 @@ func TestClientCall(t *testing.T) {
 		t.Fatalf("Decoding included tweets: %v", err)
 	}
 	for i, tweet := range tweets {
-		t.Logf("Tweet [%d]: %+v", i+1, tweet)
+		t.Logf("Tweet [%d]: id=%s", i+1, tweet.ID)
 	}
 }
 
 func TestTweetLookup(t *testing.T) {
-	cli := &twitter.Client{
-		Authorize: checkAuth(t),
-		Log:       logFunc(t),
-	}
+	checkManual(t)
+	cli := newClient(t)
 
 	ctx := context.Background()
-	// jack 1247616214769086465
 	rsp, err := tweets.Lookup("1297524288245895168", &tweets.LookupOpts{
 		TweetFields: []string{
 			types.Tweet_CreatedAt,
 			types.Tweet_Entities,
+			types.Tweet_AuthorID,
 		},
+		Expansions: []string{types.ExpandMentionUsername},
 	}).Invoke(ctx, cli)
 	if err != nil {
 		t.Fatalf("Lookup failed: %v", err)
 	}
-	t.Logf("Lookup reply: %s", string(rsp.Reply.Data))
+	t.Logf("Lookup request returned %d bytes", len(rsp.Reply.Data))
+
 	for i, v := range rsp.Tweets {
-		t.Logf("Tweet %d: %+v", i+1, v)
+		t.Logf("Tweet %d: id=%s, author=%s", i+1, v.ID, v.AuthorID)
 	}
-	its, err := rsp.IncludedTweets()
+	ius, err := rsp.IncludedUsers()
 	if err != nil {
-		t.Fatalf("Decoding included tweets: %v", err)
+		t.Fatalf("Decoding included users: %v", err)
 	}
-	for i, v := range its {
-		t.Logf("Included Tweet %d: %+v", i+1, v)
+	for i, v := range ius {
+		t.Logf("Included User %d: id=%s, username=%q, name=%q", i+1, v.ID, v.Username, v.Name)
 	}
 }
 
 func TestUserIDLookup(t *testing.T) {
-	cli := &twitter.Client{
-		Authorize: checkAuth(t),
-		Log:       logFunc(t),
-	}
+	checkManual(t)
+	cli := newClient(t)
 
 	ctx := context.Background()
-	rsp, err := users.Lookup("12", nil).Invoke(ctx, cli)
+	rsp, err := users.Lookup("12", nil).Invoke(ctx, cli) // @jack
 	if err != nil {
 		t.Fatalf("Lookup failed: %v", err)
 	}
-	t.Logf("Lookup reply: %s", string(rsp.Reply.Data))
+	t.Logf("Lookup request returned %d bytes", len(rsp.Reply.Data))
 
 	for i, v := range rsp.Users {
-		t.Logf("User %d: %+v", i+1, v)
+		t.Logf("User %d: id=%s, username=%q, name=%q", i+1, v.ID, v.Username, v.Name)
 	}
 }
 
 func TestUsernameLookup(t *testing.T) {
-	cli := &twitter.Client{
-		Authorize: checkAuth(t),
-		Log:       logFunc(t),
-	}
+	checkManual(t)
+	cli := newClient(t)
 
 	ctx := context.Background()
 	rsp, err := users.LookupByName("creachadair", &users.LookupOpts{
-		Keys: []string{"jack", "benjaminwittes"},
+		Keys: []string{"jack", "inlieuoffunshow"},
 		UserFields: []string{
 			types.User_PinnedTweetID,
 			types.User_ProfileImageURL,
@@ -129,30 +139,24 @@ func TestUsernameLookup(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Lookup failed: %v", err)
 	}
-	t.Logf("Lookup reply: %s", string(rsp.Reply.Data))
+	t.Logf("Lookup request returned %d bytes", len(rsp.Reply.Data))
 
 	for i, v := range rsp.Users {
-		t.Logf("User %d: %+v", i+1, v)
+		t.Logf("User %d: id=%s, username=%q, name=%q", i+1, v.ID, v.Username, v.Name)
+		t.Logf("User %d public metrics: %+v", i+1, v.PublicMetrics)
 	}
-}
-
-func yesterday() time.Time {
-	y, m, d := time.Now().Date()
-	return time.Date(y, m, d-1, 21, 0, 0, 0, time.UTC)
 }
 
 func TestSearchRecent(t *testing.T) {
-	cli := &twitter.Client{
-		Authorize: checkAuth(t),
-		Log:       logFunc(t),
-	}
+	checkManual(t)
+	cli := newClient(t)
 
 	ctx := context.Background()
 	const query = `from:benjaminwittes "Today on @inlieuoffunshow"`
 	rsp, err := tweets.SearchRecent(query, &tweets.SearchOpts{
 		MaxResults:  10,
-		StartTime:   yesterday(),
-		TweetFields: []string{types.Tweet_Entities},
+		StartTime:   time.Now().Add(-24 * time.Hour),
+		TweetFields: []string{types.Tweet_AuthorID, types.Tweet_Entities},
 	}).Invoke(ctx, cli)
 	if err != nil {
 		t.Fatalf("SearchRecent failed: %v", err)
@@ -161,11 +165,10 @@ func TestSearchRecent(t *testing.T) {
 	if len(rsp.Tweets) == 0 {
 		t.Fatal("No matching results")
 	}
-	for i, tweet := range rsp.Tweets {
-		t.Logf("Match %d: %+v", i+1, tweet)
-		for j, u := range tweet.Entities.URLs {
-			t.Logf("-- URL [%d]: %s", j+1, u.Expanded)
-			t.Logf("   RAW %#v", u)
+	for i, tw := range rsp.Tweets {
+		t.Logf("Match %d: id=%s, author=%s, text=%q", i+1, tw.ID, tw.AuthorID, tw.Text)
+		for j, u := range tw.Entities.URLs {
+			t.Logf("-- URL %d: (%d..%d) %s title=%q", j+1, u.Start, u.End, u.Expanded, u.Title)
 		}
 	}
 }
