c579e54acb45d787bf680d7458fa3b63909317ed
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-10-26 10:27:25 -0700

    Move shared test setup to an internal package.

---
+++ internal/otest/otest.go
@@ -0,0 +1,40 @@
+// Copyright (C) 2020 Michael J. Fromberger. All Rights Reserved.
+
+// Package otest carries some shared setup code for unit tests.
+package otest
+
+import (
+	"flag"
+	"os"
+	"testing"
+
+	"github.com/creachadair/jhttp"
+	"github.com/creachadair/twitter"
+)
+
+var (
+	doVerboseLog = flag.Bool("verbose-log", false, "Enable verbose client logging")
+)
+
+// GetOrSkip returns the value of the specified environment variable, marking t
+// as skipped if the value is unset or empty.
+func GetOrSkip(t *testing.T, key string) string {
+	t.Helper()
+	val := os.Getenv(key)
+	if val == "" {
+		t.Skip("Missing " + key + " in environment; skipping this test")
+	}
+	return val
+}
+
+// NewClient is a wrapper for twitter.NewClient that sets up debug logging if
+// enabled by the -verbose-log flag.
+func NewClient(t *testing.T, opts *jhttp.Client) *twitter.Client {
+	cli := twitter.NewClient(opts)
+	if *doVerboseLog {
+		cli.Log = func(tag jhttp.LogTag, msg string) {
+			t.Logf("DEBUG :: %s | %s", tag, msg)
+		}
+	}
+	return cli
+}
