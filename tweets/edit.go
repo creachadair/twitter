3518b75a000eb924a1de4f3f5fc1a501f2ec7438
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-01-09 16:12:52 -0800

    Separate parameters from wire encoding.

--- tweets/edit.go
+++ tweets/edit.go
@@ -4,6 +4,7 @@ package tweets
 
 import (
 	"encoding/json"
+	"time"
 
 	"github.com/creachadair/jhttp"
 	"github.com/creachadair/twitter/types"
@@ -18,32 +19,48 @@ func Post(opts PostOpts) Query {
 		HTTPMethod: "POST",
 		Params:     make(jhttp.Params),
 	}
-	data, err := json.Marshal(opts)
+	tweet := &postTweet{Text: opts.Text, QuotedID: opts.QuoteOf}
+	if opts.InReplyTo != "" {
+		tweet.Reply = &replyOpts{InReplyTo: opts.InReplyTo}
+	}
+	if len(opts.PollOptions) != 0 {
+		tweet.Poll = &pollOpts{
+			Options:  opts.PollOptions,
+			Duration: types.Minutes(opts.PollDuration),
+		}
+	}
+
+	data, err := json.Marshal(tweet)
 	req.Data = data
 	req.ContentType = "application/json"
 	return Query{Request: req, encodeErr: err}
 }
 
-// PostOpts is the encoding of data to create a new tweet.
+// PostOpts are the settings needed to post a new tweet.
 type PostOpts struct {
-	Text string `json:"text" twitter:"required"`
+	Text         string        // the text of the tweet (required)
+	QuoteOf      string        // the ID of a tweet to quote
+	InReplyTo    string        // the ID of a tweet to reply to
+	PollOptions  []string      // options to create a poll (if non-empty)
+	PollDuration time.Duration // poll duration (required with poll options)
+}
 
+type postTweet struct {
+	Text       string     `json:"text" twitter:"required"`
 	QuotedID   string     `json:"quote_tweet_id,omitempty"`
 	LimitReply string     `json:"reply_settings,omitempty"` // mentionedUsers, following
-	Poll       *PollOpts  `json:"poll,omitempty"`
-	Reply      *ReplyOpts `json:"reply,omitempty"`
+	Poll       *pollOpts  `json:"poll,omitempty"`
+	Reply      *replyOpts `json:"reply,omitempty"`
 
 	// TODO: DM links, super followers, geo, media
 }
 
-// PollOpts is the encoding of data to create a poll tweet.
-type PollOpts struct {
+type pollOpts struct {
 	Duration types.Minutes `json:"duration_minutes,omitempty"`
 	Options  []string      `json:"options"`
 }
 
-// ReplyOpts is the encoding of data to filter reply mentions.
-type ReplyOpts struct {
+type replyOpts struct {
 	InReplyTo string   `json:"in_reply_to_tweet_id,omitempty"`
 	Exclude   []string `json:"exclude_reply_user_ids,omitempty"`
 }
